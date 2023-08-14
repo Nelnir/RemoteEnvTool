fc8adf162247e5792bf0b6214d283bcf99030e1f
Marcin Dzieciatkowski <marcin.dzieciatkowski@asseco.pl>
2023-08-14 18:01:39 +0200

    Initial CMakeLists.txt config

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "FTPClient.h"
+#include "telnetpp/telnetpp.hpp"
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/program_options.hpp>
+
+int main(int argc, char** argv)
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
