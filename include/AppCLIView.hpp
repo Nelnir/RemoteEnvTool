e49e82164887f975f05c88a9ca724d4b30cf3640
Marcin Dzieciatkowski <marcin.dzieciatkowski@asseco.pl>
2023-08-28 18:17:20 +0200

    Added initial command-line parameters & Configured colors for console on WIN32

--- include/AppCLIView.hpp
+++ include/AppCLIView.hpp
@@ -2,11 +2,32 @@
 #define APP_CLI_VIEW_HPP
 
 #include "AppCLIController.hpp"
+#include <boost/program_options.hpp>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+namespace po = boost::program_options;
 
 class AppCLIView{
 public:
-    AppCLIView(int argc, char** argv);
+    AppCLIView(const int& argc, char** argv);
     void show(AppCLIController& controller);
+private:
+    void checkForUnrecognizedOptions(po::parsed_options& parsed);
+    void setRedColor();
+    void setGreenColor();
+    void setWhiteColor();
+
+    const int& argc;
+    char** argv;
+
+    po::options_description opt;
+
+#ifdef _WIN32
+HANDLE hConsole;
+#endif
 };
 
 #endif
--- src/AppCLIView.cpp
+++ src/AppCLIView.cpp
@@ -1,15 +1,73 @@
 #include "AppCLIView.hpp"
-#include <boost/program_options.hpp>
 #include <iostream>
 
-namespace po = boost::program_options;
-
-AppCLIView::AppCLIView(int argc, char** argv)
+AppCLIView::AppCLIView(const int& ar, char** av) : argc(ar), argv(av),
+opt("Allowed options")
 {
+    opt.add_options()
+    ("help", "produce help message")
+    ("host", po::value<std::string>(), "set host which to connect")
+    ;
 
+#ifdef _WIN32
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+#endif
 }
 
 void AppCLIView::show(AppCLIController& controller)
 {
-    std::cout << "CLI-VIEW\n";
+    std::vector<std::string> simulated_args = {"--host", "devcoo5"};
+
+    po::variables_map vm;
+    po::parsed_options parsed = po::command_line_parser(simulated_args).options(opt).allow_unregistered().run();
+    po::store(parsed, vm);
+    //po::store(po::parse_command_line(argc, argv, opt), vm);
+    po::notify(vm);    
+
+    checkForUnrecognizedOptions(parsed);
+
+    if (vm.count("help")) {
+        std::cout << opt << "\n";
+    }
+
+    if (vm.count("host")) {
+        std::cout << "host was set to " << vm["host"].as<std::string>() << ".\n";
+    } else {
+        std::cout << "host was not set.\n";
+    }
+}
+
+void AppCLIView::checkForUnrecognizedOptions(po::parsed_options& parsed)
+{
+    std::vector<std::string> unrecognized = po::collect_unrecognized(parsed.options, po::include_positional);
+    
+    if (!unrecognized.empty()) {
+        setRedColor();
+        std::cout << "Unrecognized options:\n";
+        for (const std::string& option : unrecognized) {
+            std::cout << option << "\n";
+        }
+        setWhiteColor();
+    }
+}
+
+void AppCLIView::setGreenColor()
+{
+#ifdef _WIN32
+    SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
+#endif
+}
+
+void AppCLIView::setRedColor()
+{
+#ifdef _WIN32
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+#endif
+}
+
+void AppCLIView::setWhiteColor()
+{
+#ifdef _WIN32
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+#endif
 }
